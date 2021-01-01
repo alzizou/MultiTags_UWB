129c78ad91d1981133eced7a81838e1833b01c92
alzizou <ali.safaei@mail.mcgill.ca>
2021-01-01 12:40:50 -0500

    adding the required elements of a gradient dscent solution for the problem of multi-tag position estimation

--- src/multitags_uwb/include/position_estimation.h
+++ src/multitags_uwb/include/position_estimation.h
@@ -3,15 +3,15 @@
 
 #endif // POSITION_ESTIMATION_H
 
-#include <stdio>
-#include <vector>
-
 class Anchor{
 	char str = 'A';
 	int ID = 100;
-	float x = 0.0;
-	float y = 0.0;
-	float z = 0.0;
+	float global_x = 0.0;
+	float global_y = 0.0;
+	float global_z = 0.0;
+	float local_x = 0.0;
+	float local_y = 0.0;
+	float local_z = 0.0;
 };
 
 class Tag{
@@ -26,48 +26,207 @@ class Tag{
 	float local_z = 0.0;
 };
 
-class Solution{
-	int Total_num_tags = 0;
-	std::vector<float> A;
-	std::vector<float> B;
-	std::vector<float> B_hat;
-	std::vector<float> e;
-	float alpha;
-	float err = 1.0e9;
-	Anchor anchor_inst;
-	Tag TagsList[];
-	void Init(int inp_num_tags, char inp_str_anch);
-	void Construct_B(void);
-	void Construct_A(void);
-	void Gradient_Descent_Solver(void);
+class Robot_Pos{
+	float global_x = 0.0;
+	float global_y = 0.0;
+	float global_z = 0.0;
+	float local_x = 0.0;
+	float local_y = 0.0;
+	float local_z = 0.0;
 };
 
 
-void Solution::Init(int inp_num_tags, Anchor inp_anch, Tag inp_TagsList){
+class Solution{
+	int Total_num_tags = 0;
+	int num_equations = 0;
+	int num_variables = 0;
+	int num_edges = 0;
+	float A;
+	float A_trans;
+	float B;
+	float B_hat;
+	float e;
+	float Gradient;
+	float x;
+	float x_dot;
+	int edges;
+	float alpha;
+	float err = 1.0e9;
+	float threshold = 1.0e0;
+	int Total_num_Itr = 1e3;
+	Anchor anchor;
+	Tag TagsList[];
+	Robot_Pos robot;
+	void Init(int inp_num_tags, char inp_str_anch);
+	void Construct_B(void);
+	void Construct_A(void);
+	void Construct_Bhat(void);
+	void Construct_e(void);
+	void Construct_Atrans(void);
+	void Construct_Gradient(void);
+	void Construct_Error_Norm(void);
+	void Gradient_Descent_Solver(void);
+	void Pos_Estimate(void);
+};
+
+
+void Solution::Init(int inp_num_tags, Anchor inp_anch, Tag inp_TagsList, float inp_alpha){
 	Total_num_tags = inp_num_tags;
-	anchor_inst = inp_anch;
+	anchor = inp_anch;
 	TagsList = inp_TagsList;
-	std::vector<float> A (Total_num_tags,0.0);
-	std::vector<float> B (Total_num_tags,0.0);
-	std::vector<float> B_hat (Total_num_tags,0.0);
-	std::vector<float> e (Total_num_tags,0.0);
+	num_edges = Total_num_tags * (Total_num_tags - 1) / 2;
+	num_variables = 3 * Total_num_tags;
+	num_equations = Total_num_tags + (3 * num_edges);
+	A[num_equations][num_variables];
+	A_trans[num_variables][num_equations];
+	B[num_equations][1];
+	B_hat[num_equations][1];
+	e[num_equations][1];
+	x[num_variables][1];
+	Gradient[num_variables][1];
+	alpha[num_variables][num_variables];
+	edges[num_equations][2];
+	int num = 0;
+	for (int i=0;i<Total_num_tags;i++){
+		for (int j=i+1;j<Total_num_tags;j++){
+			edges[num][0] = i;
+			edges[num][1] = j;
+			num = num + 1;
+			if (num == num_edges){
+				break;
+			}
+		}
+		if (num == num_edges){
+			break;
+		}
+	}
+	for (int i=0;i<num_variables;i++){
+		x[i][0] = 0.0;
+		Gradient[i][0] = 0.0;
+		x_dot[i][0] = 0.0;
+		for (int j=0;j<num_equations;j++){
+			A[j][i] = 0.0;
+			A_trans[i][j] = 0.0;
+		}
+		for (int j=0;j<num_variables;j++){
+			alpha[i][j] = 0.0;
+		}
+		alpha[i][i] = inp_alpha;
+	}
+	Construct_B();
+	robot.local_x = 0.0;
+	robot.local_y = 0.0;
+	robot.local_z = 0.0;
 }
 
 
 void Solution::Construct_B(void){
 	for (int i=0;i<Total_num_tags;i++){
-		B.at(i) = (TagsList[i].dist ** 2) - (anchor_inst.x ** 2) - (anchor_inst.y ** 2) - (anchor_inst.z ** 2);
+		B[i][0] = (TagsList[i].dist ** 2) - (anchor.global_x ** 2) - /
+		                (anchor.global_y ** 2) - (anchor.global_z ** 2);
+	}
+	int num = Total_num_tags + 1;
+	for (int i=0;i<num_edges;i++){
+		num = num + (i*3);
+		B[num+0][0] = TagsList[edges[i][1]].local_x - TagsList[edges[i][0]].local_x;
+		B[num+1][0] = TagsList[edges[i][1]].local_y - TagsList[edges[i][0]].local_y;
+		B[num+2][0] = TagsList[edges[i][1]].local_z - TagsList[edges[i][0]].local_z;
 	}
 
-
 }
+
 
 void Solution::Construct_A(void){
-
+	for (int i=0;i<Total_num_tags;i++){
+		A[i][(i*3)+0] = x[i*3+0][0] - (2 * anchor.global_x);
+		A[i][(i*3)+1] = x[i*3+1][0] - (2 * anchor.global_y);
+		A[i][(i*3)+2] = x[i*3+2][0] - (2 * anchor.global_z);
+	}
+	int num = Total_num_tags + 1;
+	for (int i=0;i<num_edges;i++){
+		num = num + (i*3);
+		A[num+0][(edges[i][0]*3)+0] = -1.0;
+		A[num+0][(edges[i][1]*3)+0] = +1.0;
+		A[num+1][(edges[i][0]*3)+1] = -1.0;
+		A[num+1][(edges[i][1]*3)+1] = +1.0;
+		A[num+2][(edges[i][0]*3)+2] = -1.0;
+		A[num+2][(edges[i][1]*3)+2] = +1.0;
+	}
 }
+
+
+void Solution::Construct_Bhat(void){
+	for (int i=0;i<num_equations;i++){
+		B_hat[i][0] = 0;
+		for (int j=0;j<num_variables;j++){
+			B_hat[i][0] = B_hat[i][0] + (A[i][j] * x[j][0]);
+		};
+	};
+}
+
+
+void Solution::Construct_e(void){
+	for (int i=0;i<num_equations;i++){
+		e[i][0] = B[i][0] - B_hat[i][0];
+	};
+}
+
+
+void Solution::Construct_Atrans(void){
+	for (int i=0;i<num_equations;i++){
+		for (int j=0;j<num_variables;j++){
+			A_trans[j][i] = A[i][j];
+		}
+	}
+}
+
+
+void Solution::Construct_Gradient(void){
+	for (int i=0;i<num_variables;i++){
+		Gradient[i][0] = 0;
+		for (int j=0;j<num_equations;j++){
+			Gradient[i][0] = Gradient[i][0] + (A_trans[i][j] * e[j][0]);
+		};
+	};
+}
+
+
+void Solution::Construct_Error_Norm(void){
+	err = 0.0;
+	for (int i=0;i<num_equations;i++){
+		err = err + (e[i][0] * e[i][0]);
+	};
+}
+
 
 void Solution::Gradient_Descent_Solver(void){
+	int num_itr = 0;
+	while (err > threshold){
+		Construct_A();
+		Construct_Bhat();
+		Construct_e();
+		Construct_Atrans();
+		Construct_Gradient();
 
+		for (int i=0;i<num_variables;i++){
+			    x_dot[i][0] = 0;
+			    for (int j=0;j<num_variables;j++){
+				x_dot[i][0] = x_dot[i][0] - (alpha[i][j] * Gradient[j][0]);
+			    };
+			    x[i][0] = x[i][0] + x_dot[i][0];
+		};
+
+		Construct_Error_Norm();
+		if (num_itr > Total_num_Itr){
+			break;
+		}
+		num_itr = num_itr + 1;
+	}
 }
 
+
+
+void Solution::Pos_Estimate(void){
+
+}
 
--- src/multitags_uwb/src/position_estimation.cpp
+++ src/multitags_uwb/src/position_estimation.cpp
@@ -9,6 +9,52 @@
 #include <numeric>
 #include <geometry_msgs/Pose.h>
 #include <geometry_msgs/Quaternion.h>
+#include "position_estimation.h"
 
+int num_tags = 3;
+float GD_alpha = 1.0e-3;
+Solution solution_inst;
+Tag All_tags[num_tags];
+Anchor The_anchor;
+geometry_msgs::Pose Robot_Absolute_Pose;
+geometry_msgs::Pose Anchor_Relative_Pose;
 
+void callback_dists(std_msgs::String msg){
+
+}
+
+int main(int argc, char **argv){
+	ros::init(argc,argv,"position_estimation");
+	ros::NodeHandle n;
+	ros::Rate loop_rate(100.0);
+	ros::Subscriber sub_dists = n.subscribe("ALL_DIST",10,callback_dists);
+	ros::Publisher pub_absolute_pos_robot = n.advertise<geometry_msgs::Pose>("Robot_Absolute_Pose",10);
+	ros::Publisher pub_relative_pos_anchor = n.advertise<geometry_msgs::Pose>("Anchor_Relative_Pose",10);
+	ros::Time time_stmp = ros::Time::now();
+	solution_inst.Init(num_tags, The_anchor, All_tags, GD_alpha);
+
+	while (ros::ok()){
+		//-----------------------------------------------------------------------------------------------
+		//(START) measuring the frequency of the localziation estimation process
+		long double period = (ros::Time::now() - time_stmp).toSec();
+		time_stmp = ros::Time::now();
+		long double frequency = 1.0/period;
+		//(END) measuring the frequency of the localziation estimation process
+		//---------------------------------------------------------------------------------------
+		//(START) main computations and publishing estimated positions
+		solution_inst.Gradient_Descent_Solver();
+		Robot_Absolute_Pose.position.x = solution_inst.robot.global_x;
+		Robot_Absolute_Pose.position.y = solution_inst.robot.global_y;
+		Robot_Absolute_Pose.position.z = solution_inst.robot.global_z;
+		Anchor_Relative_Pose.position.x = solution_inst.anchor_inst.local_x;
+		Anchor_Relative_Pose.position.y = solution_inst.anchor_inst.local_y;
+		Anchor_Relative_Pose.position.z = solution_inst.anchor_inst.local_z;
+		pub_absolute_pos_robot.publish(Robot_Absolute_Pose);
+		pub_relative_pos_anchor.publish(Anchor_Relative_Pose);
+		//(END) main computations and publishing estimated positions
+		//---------------------------------------------------------------------------------------------
+		ros::spinOnce();
+		loop_rate.sleep();
+	}
+}
 
